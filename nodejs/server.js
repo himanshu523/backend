f2ef6b847822042fbfcfc953063be6ef4ece20b7
himanshuranjan.y@outlook.com <himanshuranjan.y@outlook.com>
2023-01-08 10:08:59 +0530

    request response

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,12 +2,53 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>
 {
-    console.log(req);
-    console.log('himanshu')
-    res.writeHead(200,{
-        'content-type':'text/plain'
-    });
-    res.end('himanshu')
+    console.log(req.url,req.headers,req.method);
+    const url =req.url;
+    if(url==='/')
+    {
+    res.setHeader('content-type','text/html');
+    res.write('<html>')
+    res.write('<head><title>server</title></head>')
+    res.write('<body>')
+    res.write('<h3>Welcome to my Node Js project</h3>')
+    res.write('</body>')
+    res.write('</html>')
+    return res.end()
+    }
+
+    if(url==='/home')
+    {
+    res.setHeader('content-type','text/html');
+    res.write('<html>')
+    res.write('<head><title>server</title></head>')
+    res.write('<body>')
+    res.write('<h3>Welcome home</h3>')
+    res.write('</body>')
+    res.write('</html>')
+    return res.end()
+    }
+    else if(url==='/about')
+    {
+    res.setHeader('content-type','text/html');
+    res.write('<html>')
+    res.write('<head><title>server</title></head>')
+    res.write('<body>')
+    res.write('<h3>Welcome to about us page</h3>')
+    res.write('</body>')
+    res.write('</html>')
+    return res.end()
+    }
+    else if(url==='/node')
+    {
+    res.setHeader('content-type','text/html');
+    res.write('<html>')
+    res.write('<head><title>server</title></head>')
+    res.write('<body>')
+    res.write('<h3>Welcome to nodejs project</h3>')
+    res.write('</body>')
+    res.write('</html>')
+   return  res.end()
+    }
 })
 
 server.listen(4000);
